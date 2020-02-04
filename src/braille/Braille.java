8441e8950f5e7cbb9d3020bfd022aadd241a91f5
CRG15@students.ptcollege.edu <>
2020-02-04 15:27:47 -0500

    this is the first commit

---
+++ src/braille/Braille.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package braille;
+
+/**
+ *
+ * @author mikun
+ */
+public class Braille {
+    
+    public static void main(String[] args) {
+        String C = "\u280C";
+        String A = "\u280A";
+        String L = "\u2807";
+        String E = "\u280E";
+        String B = "\u280B";
+        
+        System.out.println(C + A + L + E + B);
+    }
+    
+}
